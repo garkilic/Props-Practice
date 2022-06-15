9e680a33975a85d79c6c5eaf6613c2fd24a14fb8
Griffin Arkilic <garkilic@gmail.com>
2022-06-15 10:38:04 -0700

    React Practice

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<h1>The Mini Blog</h1>
+			<a href="/">Home</a>
+			<a href="/create">Create</a>
+		</nav>
+	);
+};
+
+export default Navbar;
